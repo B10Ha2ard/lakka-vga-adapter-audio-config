120c033d4551edaa624271ac3260823a07c981aa
Daniel Kotze <daniel.kotze@gmail.com>
2018-02-21 00:40:18 +0200

    Documentation: Updated documentation with instuctions. Script update: Changed append path of the installation script.

--- source/install_sound_environment_configuration.sh
+++ source/install_sound_environment_configuration.sh
@@ -4,11 +4,11 @@ mount -o remount,rw /flash
 
 # Append audio configuration section to the main Raspberry Pi configuration file.
 
-printf "\n\n"
+printf "\n\n" >> /flash/config.txt
 echo "################################################################################" >> /flash/config.txt
 echo "# Audio configuration for specific environment." >> /flash/config.txt
 echo "################################################################################" >> /flash/config.txt
-printf "\n"
-echo "include environment_audio_setup_config.txt" >> /flash/environment_audio_setup_config.txt
+printf "\n" >> /flash/config.txt
+echo "include environment_audio_setup_config.txt" >> /flash/config.txt
 
 mount -o remount,ro /flash
